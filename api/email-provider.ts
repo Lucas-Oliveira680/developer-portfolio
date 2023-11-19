bbfcf717a3fb06fe6777a1e5b113c14a2e0322cb
Lucas Estevão De Oliveira <luccaseoliveira@gmail.com>
2023-11-19 11:36:57 -0300

    refactor: fix email provider bug

--- api/email-provider.ts
+++ api/email-provider.ts
@@ -1,17 +1,17 @@
-import emailjs from 'emailjs-com'
+import { send } from '@emailjs/nodejs'
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-
-emailjs.init(process.env['EMAILJS_USER_ID']!)
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const emailData = req.body
 
-  emailjs
-    .send(process.env['EMAILJS_SERVICE_ID']!, process.env['EMAILJS_TEMPLATE_ID']!, emailData)
-    .then((response: any) => {
+  send(process.env['EMAILJS_SERVICE_ID']!, process.env['EMAILJS_TEMPLATE_ID']!, emailData, {
+    publicKey: process.env['EMAILJS_PUBLIC_KEY']!,
+    privateKey: process.env['EMAILJS_PRIVATE_KEY']!,
+  })
+    .then(() => {
       res.status(200).json({ message: 'Email sent successfully' })
     })
-    .catch((error: any) => {
+    .catch(error => {
       res.status(500).json({ message: 'Failed to send email', error })
     })
 }
