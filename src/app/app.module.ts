a1e65910c6df829b5662dcb0e8325d06a15b9a70
Lucas Estevão De Oliveira <luccaseoliveira@gmail.com>
2023-11-20 16:21:39 -0300

    delete: remove three
    delete: remove three
    
    delete: remove three

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {RouterOutlet} from "@angular/router";
-import {NavbarComponent} from "src/app/shared/components/navbar/navbar.component";
-import { HomeComponent } from './pages/home/home.component';
-import { ProjectsComponent } from './pages/projects/projects.component';
-import {ThemeToggleComponent} from "src/app/shared/components/theme-toggle/theme-toggle.component";
-import {TypewriterDirective} from "src/app/shared/directives/typewriter.directive";
-import {EarthComponent} from "src/app/shared/components/earth/earth.component";
-import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
-import {HttpClient, HttpClientModule} from "@angular/common/http";
-import {TranslateHttpLoader} from "@ngx-translate/http-loader";
-import {MobileNavbarComponent} from "src/app/shared/components/mobile-navbar/mobile-navbar.component";
-import {LanguageToggleComponent} from "src/app/shared/components/language-toggle/language-toggle.component";
-import {WorkExperienceComponent} from "src/app/pages/projects/work-experience/work-experience.component";
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { RouterOutlet } from '@angular/router'
+import { NavbarComponent } from 'src/app/shared/components/navbar/navbar.component'
+import { HomeComponent } from './pages/home/home.component'
+import { ProjectsComponent } from './pages/projects/projects.component'
+import { ThemeToggleComponent } from 'src/app/shared/components/theme-toggle/theme-toggle.component'
+import { TypewriterDirective } from 'src/app/shared/directives/typewriter.directive'
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core'
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
+import { MobileNavbarComponent } from 'src/app/shared/components/mobile-navbar/mobile-navbar.component'
+import { LanguageToggleComponent } from 'src/app/shared/components/language-toggle/language-toggle.component'
+import { WorkExperienceComponent } from 'src/app/pages/projects/work-experience/work-experience.component'
 
 export function HttpLoaderFactory(http: HttpClient) {
-    return new TranslateHttpLoader(http, 'assets/i18n/');
+  return new TranslateHttpLoader(http, 'assets/i18n/')
 }
+
 @NgModule({
   declarations: [AppComponent, HomeComponent, ProjectsComponent],
   imports: [
@@ -29,7 +29,6 @@ export function HttpLoaderFactory(http: HttpClient) {
     NavbarComponent,
     ThemeToggleComponent,
     TypewriterDirective,
-    EarthComponent,
     HttpClientModule,
     TranslateModule.forRoot({
       loader: {
